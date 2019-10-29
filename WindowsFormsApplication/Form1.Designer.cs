6b4b55eff6cf4b140fb4e2fbd2bbbd4a9fefa7f0
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-10-29 22:24:39 +0200

    Add files via upload

---
+++ WindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace GreatMp3Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoadFiles = new System.Windows.Forms.Button();
+            this.btnPlayAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoadFiles
+            // 
+            this.btnLoadFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFiles.Name = "btnLoadFiles";
+            this.btnLoadFiles.Size = new System.Drawing.Size(149, 48);
+            this.btnLoadFiles.TabIndex = 0;
+            this.btnLoadFiles.Text = "Load Song";
+            this.btnLoadFiles.UseVisualStyleBackColor = true;
+            this.btnLoadFiles.Click += new System.EventHandler(this.btnLoadFiles_Click);
+            // 
+            // btnPlayAll
+            // 
+            this.btnPlayAll.Location = new System.Drawing.Point(12, 508);
+            this.btnPlayAll.Name = "btnPlayAll";
+            this.btnPlayAll.Size = new System.Drawing.Size(149, 48);
+            this.btnPlayAll.TabIndex = 1;
+            this.btnPlayAll.Text = "Play All";
+            this.btnPlayAll.UseVisualStyleBackColor = true;
+            this.btnPlayAll.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(12, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(491, 436);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 68;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Location";
+            this.columnHeader2.Width = 132;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Author";
+            this.columnHeader3.Width = 102;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Genre";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(509, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(472, 544);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Delete Song";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 508);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Play Random";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(104, 146);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 266);
+            this.panel1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(161, 196);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 26);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "cancel";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 26);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Genre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Singer or Band";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Song Title";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Blues",
+            "Hip hop",
+            "Jazz",
+            "Pop",
+            "Rap",
+            "Rock",
+            ""});
+            this.comboBox1.Location = new System.Drawing.Point(15, 194);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(186, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 48);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Update Song";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnPlayAll);
+            this.Controls.Add(this.btnLoadFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mp3Paixtis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFiles;
+        private System.Windows.Forms.Button btnPlayAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApplication/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace GreatMp3Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            panel1.Visible = false;//this panel will open every time we want to load new song or update a song's info
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Play all button
+        {
+            if (listView1 != null)//there must be at least 1 song
+            { 
+                WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");//i create a playlist item
+                WMPLib.IWMPMedia media;//and a media item
+                for (int i = 0; i < listView1.Items.Count; i++)//for all the songs
+                {
+                    media = axWindowsMediaPlayer1.newMedia(listView1.Items[i].SubItems[1].Text);//we get the 2nd subitem of each row which is the full path
+                    playlist.appendItem(media);//we create the playlist by adding all the paths
+                    
+                }
+                axWindowsMediaPlayer1.currentPlaylist = playlist;//we initialize the playlist with the one we just created
+                axWindowsMediaPlayer1.Ctlcontrols.play();//we play the playlist
+            }
+
+        }
+
+        string[] saLvwItem = new string[4];
+        int bt = 0;//this will be 1 if we clicked load song or -1 if we clicked update song
+        int counter;//this will count the number of the songs we have checked for duplicate song
+        private void btnLoadFiles_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            //we open a dialog window to choose one audio file
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "Mp3 Audio File(*.mp3)|*.mp3|Windows Media Audio File(*.wma)|*.wma|WAV Audio file(*.wav)|*.wav|All files(*.*)|*.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)//if we chose a valid file
+                {
+                    for(int i=0; i<listView1.Items.Count; i++)//for all the songs we have loaded we check if we have chosen the same
+                    {
+                        if (listView1.Items[i].SubItems[1].Text == ofd.FileName)
+                        {
+                            MessageBox.Show("You already have this song in your playlist!");
+                        }
+                        else
+                            counter++;//if the song we chose isn't in our playlist the counter should be equal to the number of the total songs
+                    }
+                    if (counter == listView1.Items.Count)
+                    {
+                        saLvwItem[1] = ofd.FileName;//we save the file's whole path
+                        saLvwItem[0] = Path.GetFileNameWithoutExtension(saLvwItem[1]);//we get the name of the file without extensions
+                        //we open a panel where the user will be able to fill each song's information
+                        bt = 1;//this means we clicked the load file button
+                        panel1.Visible = true;//we show the panel
+                        button5.Visible = false;//we disable the form's objects
+                        textBox1.Text = saLvwItem[0];//you can change the name
+                        textBox2.Clear();
+                        comboBox1.SelectedItem = "";
+                        button1.Enabled = false;
+                        button2.Enabled = false;
+                        button4.Enabled = false;
+                        btnLoadFiles.Enabled = false;
+                        btnPlayAll.Enabled = false;
+                        axWindowsMediaPlayer1.Ctlenabled = false;
+                    }
+                    
+                }              
+            }           
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)//double click name to play song
+        {
+            string selectedFile = listView1.FocusedItem.SubItems[1].Text;
+            axWindowsMediaPlayer1.URL = @selectedFile;
+        }
+
+        private void button3_Click(object sender, EventArgs e)//the panel save button
+        {
+            if (bt == 1)//we clicked load song
+            {
+                if (textBox1.Text == "" || textBox2.Text == "" || comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Please complete the song's information");
+                }
+                else
+                {
+                    saLvwItem[0] = textBox1.Text;//title
+                    saLvwItem[2] = textBox2.Text;//author
+                    saLvwItem[3] = comboBox1.SelectedItem.ToString();//genre
+                    ListViewItem lvi = new ListViewItem(saLvwItem);
+
+                    listView1.Items.Add(lvi);//we add the new item
+
+                    panel1.Visible = false;//we enable the form's objects
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button4.Enabled = true;
+                    btnLoadFiles.Enabled = true;
+                    btnPlayAll.Enabled = true;
+                    axWindowsMediaPlayer1.Ctlenabled = true;
+
+                    InfoSaver();//we call this method that will save the new song into our database
+                }
+            }
+            else if(bt ==-1)//we clicked update song
+            {//same as the previous if
+                if (textBox1.Text == "" || textBox2.Text == "" || comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Please complete the song's information");
+                }
+                else
+                {
+                    saLvwItem[0] = textBox1.Text;
+                    saLvwItem[2] = textBox2.Text;
+                    saLvwItem[3] = comboBox1.SelectedItem.ToString();
+                    string connectionstring = "Provider=Microsoft.Jet.OleDb.4.0; Data Source=Music.mdb";//an extra thing we need to do is to update our databe 
+                    OleDbConnection conn = new OleDbConnection(connectionstring);
+                    conn.Open();
+                    string query = "Update Table1 set Title='" + textBox1.Text + "' where Path='" + uppath + "'";
+                    string query1 = "Update Table1 set Author='" + textBox2.Text + "' where Path='" + uppath + "'";
+                    string query2 = "Update Table1 set Genre='" + comboBox1.SelectedItem.ToString() + "' where Path='" + uppath + "'";
+                    OleDbCommand cmd = new OleDbCommand(query, conn);
+                    int i = cmd.ExecuteNonQuery();
+                    OleDbCommand cmd1 = new OleDbCommand(query1, conn);
+                    int i1 = cmd1.ExecuteNonQuery();
+                    OleDbCommand cmd2 = new OleDbCommand(query2, conn);
+                    int i2 = cmd2.ExecuteNonQuery();
+                    conn.Close();                   
+
+                    panel1.Visible = false;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button4.Enabled = true;
+                    btnLoadFiles.Enabled = true;
+                    btnPlayAll.Enabled = true;
+                    axWindowsMediaPlayer1.Ctlenabled = true;                    
+                    Loader();//we call this method that will clear the listview items and add them again with the changes
+                }
+            }
+        }
+
+        public void InfoSaver()//a method that saves new songs into the database
+        {
+            string connectionstring = "Provider=Microsoft.Jet.OleDb.4.0; Data Source=Music.mdb";
+            OleDbConnection conn = new OleDbConnection(connectionstring);
+            conn.Open();
+            string query = "Insert Into Table1(Path, Title, Author, Genre) values('" + saLvwItem[1] + "','" + saLvwItem[0] + "','" + saLvwItem[2] + "','" + saLvwItem[3] + "')";
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            int i = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            Loader();
+        }
+
+        public void Loader()//method that will clear the listview items and add them again with the changes
+        {
+            listView1.Items.Clear();
+            string connectionstring = "Provider=Microsoft.Jet.OleDb.4.0; Data Source=Music.mdb";
+            OleDbConnection conn = new OleDbConnection(connectionstring);
+            conn.Open();
+            string query = "SELECT * FROM Table1";
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            OleDbDataReader rdr = cmd.ExecuteReader();
+            StringBuilder builder = new StringBuilder();
+            while (rdr.Read())
+            {
+                saLvwItem[0] = rdr.GetString(2);
+                saLvwItem[1] = rdr.GetString(1);
+                saLvwItem[2] = rdr.GetString(3);
+                saLvwItem[3] = rdr.GetString(4);
+                ListViewItem lvi = new ListViewItem(saLvwItem);
+
+                listView1.Items.Add(lvi);
+            }
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Loader();//when  we open the app we load any songs that may be in our database
+        }
+        string d;//we will save the path that is to be deleted
+        private void button1_Click(object sender, EventArgs e)//delete
+        {
+            if (listView1.FocusedItem != null)//if there is a chosen item
+            {
+                try//there may be an error if we delete a song taht it is also played by the windows media player
+                {
+                    d = listView1.FocusedItem.SubItems[1].Text;
+                    WMPLib.IWMPControls3 controls = (WMPLib.IWMPControls3)axWindowsMediaPlayer1.Ctlcontrols;
+                    controls.stop();//we stop the music
+                    listView1.Items.Remove(listView1.FocusedItem);//we remove the item from the listview
+                    //and we delete the item from the database
+                    string connectionstring = "Provider=Microsoft.Jet.OleDb.4.0; Data Source=Music.mdb";
+                    OleDbConnection conn = new OleDbConnection(connectionstring);
+                    conn.Open();
+                    string query = "Delete from Table1 where Path='" + d + "'";
+                    OleDbCommand cmd = new OleDbCommand(query, conn);
+                    int i = cmd.ExecuteNonQuery();
+                    conn.Close();
+                    axWindowsMediaPlayer1.Ctlenabled = true;
+                }catch
+ {}
+            }
+        }
+
+        string uppath;//the path that is to be updated or deleted 
+        private void button4_Click(object sender, EventArgs e)//update
+        {
+            if(listView1.FocusedItem !=null)
+            {
+                bt = -1;//this means we have clicked the update button
+                panel1.Visible = true;
+                textBox1.Text = listView1.FocusedItem.SubItems[0].Text;//you can change the name
+                textBox2.Text = listView1.FocusedItem.SubItems[2].Text;//you can change the author           
+                comboBox1.SelectedItem = listView1.FocusedItem.SubItems[3].Text;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                btnLoadFiles.Enabled = false;
+                btnPlayAll.Enabled = false;
+                axWindowsMediaPlayer1.Ctlenabled = false;
+            }          
+        }
+
+        private void button5_Click(object sender, EventArgs e)//cancel button inside panel
+        {
+            panel1.Visible = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button4.Enabled = true;
+            btnLoadFiles.Enabled = true;
+            btnPlayAll.Enabled = true;
+            axWindowsMediaPlayer1.Ctlenabled = true;
+        }
+
+        private void listView1_Click(object sender, EventArgs e)//selected item
+        {
+            uppath = listView1.FocusedItem.SubItems[1].Text;//the path that is to be updated or deleted
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)//random song
+        {
+            if (listView1 != null)//if there are any songs
+            {
+                WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");//we create a new playlist 
+                WMPLib.IWMPMedia media;
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    media = axWindowsMediaPlayer1.newMedia(listView1.Items[i].SubItems[1].Text);
+                    playlist.appendItem(media);
+                }
+
+                axWindowsMediaPlayer1.currentPlaylist = playlist;
+                axWindowsMediaPlayer1.settings.setMode("shuffle", true);//with this seeting every time we click the random button or the next button a random song from our playlist will play
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+    }
+}
